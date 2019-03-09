e09b1d7167bfd8b28ea9cd8c099f30773855d7a3
Carlos Denarde <carlosv.denarde@gmail.com>
2019-03-08 21:29:48 -0300

    fix controller

--- src/main/java/com/denarde/finance/webclient/controller/AppController.java
+++ src/main/java/com/denarde/finance/webclient/controller/AppController.java
@@ -2,11 +2,13 @@ package com.denarde.finance.webclient.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
@@ -14,7 +16,6 @@ import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import com.denarde.finance.webclient.dto.ExpenseDTO;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Controller
 public class AppController {
@@ -46,11 +47,17 @@ public class AppController {
     
     @PostMapping("/app/salvar")
 	public String salvar(@Valid ExpenseDTO expense) {
-    	client.post()
-                .uri("/expenses")
-                .body(BodyInserters.fromObject( expense ) )
-                .retrieve()
-                .bodyToMono(ExpenseDTO.class);
+//    	Mono<ExpenseDTO> expenseMono = client.post()
+//                .uri("/expenses")
+//                .body(BodyInserters.fromObject( expense ) )
+//                .retrieve()
+//                .bodyToMono(ExpenseDTO.class);
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+		HttpEntity<ExpenseDTO> request = new HttpEntity<ExpenseDTO>(expense);
+		restTemplate.postForObject("https://expense-client.herokuapp.com/client/expenses", request, ExpenseDTO.class);
     	
 		return "redirect:/app";
 	}
